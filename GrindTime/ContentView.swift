a73ca80101470dcee35649e765abc59765c72eaa
dannyberry <dberry@vassar.edu>
2024-07-17 19:27:01 -0400

    Initial Commit

---
+++ GrindTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrindTime
+//
+//  Created by Danny Berry on 7/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrindTime/GrindTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrindTimeApp.swift
+//  GrindTime
+//
+//  Created by Danny Berry on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GrindTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
